ecca8ea8c2bdb7c1c657c54fa0b76af933a58b20
patmillar2 <33300696+patmillar2@users.noreply.github.com>
2018-07-26 20:22:28 -0500

    Update stepper.js
    idk

--- js/stepper.js
+++ js/stepper.js
@@ -1,24 +1,20 @@
 function switchStep(newStep)
 {
-  d3.selectAll(".step-link").classed("active", false);
-  d3.select("#" + newStep).classed("active", true);
+  $(".step-link").toggleClass("active", false);
+  $("#" + newStep).toggleClass("active", true);
 }
 
 function switchAnnotation(newStep)
 {
-  d3.selectAll(".annotation-step")
-    .style("display", "none")
-    .style("opacity", 0.0);
-
-  d3.select("#" + newStep + "-annotation")
-    .style("display", "block")
-    .transition().delay(300).duration(500)
-      .style("opacity", 1);
+  $(".annotation-step").hide();
+  $("#" + newStep + "-annotation").delay(300).fadeIn(500);
 }
 
-d3.selectAll("a.step-link").on("click", function(d) {
-  var clickedStep = d3.select(this).attr("id");
-  switchStep(clickedStep);
-  switchAnnotation(clickedStep);
-  return false;
-});
+$(document).ready(function() {
+  $("a.step-link").click(function(e) {
+    var clickedStep = $(this).attr('id');
+    switchStep(clickedStep);
+    switchAnnotation(clickedStep);
+    return false;
+  });
+})
