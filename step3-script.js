2cba38f9d93aaabd89f6dabfc427263de6bba20a
patmillar2 <33300696+patmillar2@users.noreply.github.com>
2018-07-28 20:10:25 -0500

    Update step3-script.js

--- step3-script.js
+++ step3-script.js
@@ -1,70 +1,55 @@
 !(function (d3) {
 
-$("ccontent").empty();
+$("bcontent").empty();
 
 var margin = {top: 20, right: 20, bottom: 30, left: 20},
     width = 960 - margin.left - margin.right,
     height = 700 - margin.top - margin.bottom;
 
+var svg = d3.select("bcontent").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]);
+
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 // get the data
 d3.csv("./CSV/Both.csv", function(error, data) {
 if (error) throw error;
 
+// format the data
 data.forEach(function(d) {
-    d.Rate = +d.Rate;
-    d.dataset = +d.dataset
+    d.dataset = +d.dataset;
 });
-	
-   var x = d3.scale.linear()
-              .domain([0, d3.max(data, function(d) { return d.dataset; })])
-              .range([ 0, width ]);
-    
-    var y = d3.scale.linear()
-    	      .domain([0, d3.max(data, function(d) { return d.Rate; })])
-    	      .range([ height, 0 ]);
- 
-    var chart = d3.select('ccontent')
-	.append('svg:svg')
-	.attr('width', width + margin.right + margin.left)
-	.attr('height', height + margin.top + margin.bottom)
-	.attr('class', 'chart')
 
-    var main = chart.append('g')
-	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-	.attr('width', width)
-	.attr('height', height)
-	.attr('class', 'main')   
-        
-    // draw the x axis
-    var xAxis = d3.svg.axis()
-	.scale(x)
-	.orient('bottom');
+  x.domain(data.map(function(d) { return d.Country; }));
+  y.domain([0, d3.max(data, function(d) { return d.dataset; })]);
 
-    main.append('g')
-	.attr('transform', 'translate(0,' + height + ')')
-	.attr('class', 'main axis date')
-	.call(xAxis);
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(10, "%"))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("HDI Index");
 
-    // draw the y axis
-    var yAxis = d3.svg.axis()
-	.scale(y)
-	.orient('left');
-
-    main.append('g')
-	.attr('transform', 'translate(0,0)')
-	.attr('class', 'main axis date')
-	.call(yAxis);
-
-    var g = main.append("svg:g"); 
-    
-    g.selectAll("scatter-dots")
-      .data(data)
-      .enter().append("svg:circle")
-          .attr("cx", function (d,i) { return x(d.dataset); } )
-          .attr("cy", function (d) { return y(d.Rate); } )
-          .attr("r", 4);
-	  .append("svg:title")
-      	  .text(function(d) { return d.Country; });
-  });
+  g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.Country); })
+      .attr("y", function(d) { return y(d.dataset); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.dataset); })
+      .append("svg:title")
+      .text(function(d) { return d.Country; });
+});
 
 })(d3);
