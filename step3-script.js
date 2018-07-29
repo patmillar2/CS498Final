c730f6cad960b4a403a09ea559ce7f802df2dc27
patmillar2 <33300696+patmillar2@users.noreply.github.com>
2018-07-28 20:33:19 -0500

    Update step3-script.js

--- step3-script.js
+++ step3-script.js
@@ -2,9 +2,31 @@
 
 $("ccontent").empty();
 
-var margin = {top: 20, right: 20, bottom: 30, left: 20},
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var color = d3.scale.category10();
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var svg = d3.select("ccontent").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // get the data
 d3.csv("./CSV/Both.csv", function(error, data) {
@@ -15,55 +37,39 @@ data.forEach(function(d) {
     d.dataset = +d.dataset
 });
 	
-    var x = d3.scaleLinear().rangeRound([width, 0]);
-    
-    var y = d3.scaleLinear().rangeRound([height, 0]);
- 
-    x.domain([0, d3.max(data, function(d) { return d.dataset; })]);
-    y.domain([0, d3.max(data, function(d) { return d.Rate; })]);
-	
-    var chart = d3.select('ccontent')
-	.append('svg:svg')
-	.attr('width', width + margin.right + margin.left)
-	.attr('height', height + margin.top + margin.bottom)
-	.attr('class', 'chart')
+  x.domain(d3.extent(data, function(d) { return d.dataset; })).nice();
+  y.domain(d3.extent(data, function(d) { return d.Rate; })).nice();
 
-    var main = chart.append('g')
-	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-	.attr('width', width)
-	.attr('height', height)
-	.attr('class', 'main')   
-        
-    // draw the x axis
-    var xAxis = d3.svg.axis()
-	.scale(x)
-	.orient('bottom');
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Sepal Width (cm)");
 
-    main.append('g')
-	.attr('transform', 'translate(0,' + height + ')')
-	.attr('class', 'main axis date')
-	.call(xAxis);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Sepal Length (cm)")
 
-    // draw the y axis
-    var yAxis = d3.svg.axis()
-	.scale(y)
-	.orient('left');
-
-    main.append('g')
-	.attr('transform', 'translate(0,0)')
-	.attr('class', 'main axis date')
-	.call(yAxis);
-
-    var g = main.append("svg:g"); 
-    
-    g.selectAll("scatter-dots")
+  svg.selectAll(".dot")
       .data(data)
-      .enter().append("svg:circle")
-          .attr("cx", function (d,i) { return x(d.dataset); } )
-          .attr("cy", function (d) { return y(d.Rate); } )
-          .attr("r", 4)
-	  .append("svg:title")
-      	  .text(function(d) { return d.Country; });
-  });
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", function(d) { return x(d.dataset); })
+      .attr("cy", function(d) { return y(d.Rate); })
+      .style("fill", function(d) { return color(d.Country); });
+});    
 
 })(d3);
